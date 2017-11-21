b142fbac0a0fc01fedc52c6d29b207201d4f0432
Edda Wong <wang.chao15@zte.com.cn>
2017-11-21 17:07:46 +0800

    引入element ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
-
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
